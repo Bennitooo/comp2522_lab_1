8b23476f116348113233b3f12fdd7a4d3a755992
Abdullah Munawar <abmunawar8@gmail.com>
2026-01-21 17:19:42 -0800

    slightly modified the BankClient class

--- src/code/ca/bcit/comp2522/bank/BankClient.java
+++ src/code/ca/bcit/comp2522/bank/BankClient.java
@@ -18,36 +18,35 @@ public class BankClient
      * This is a constructor for the clients of a bank.
      * @param name This is the first parameter for a name
      * @param birthDate This is the second parameter for a birthdate
-     * @param deathDate This is the third parameter for a death date
-     * @param signupDate This is the fourth parameter for a signup date
-     * @param clientID This is the fifth parameter for a client id
+     * @param signupDate This is the third parameter for a signup date
+     * @param clientID This is the fourth parameter for a client id
+     * @param deathDate This is the fifth parameter for a death date
      */
-    public BankClient(final Name name, final Date birthDate, final Date deathDate, final Date signupDate, final String clientID)
+    public BankClient(final Name name, final Date birthDate, final Date signupDate, final String clientID, final Date deathDate)
     {
         validateClientID(clientID);
         
         this.name = name;
         this.birthDate = birthDate;
-        this.deathDate = deathDate;
         this.signupDate = signupDate;
         this.clientID = clientID;
+        this.deathDate = deathDate;
     }
 
     /**
      * This is an overriding constructor for the clients of a bank who have not died.
      * @param name This is the first parameter for a name
      * @param birthDate This is the second parameter for a birthdate
-     * @param deathDate This is the third parameter for a death date
-     * @param signupDate This is the fourth parameter for a signup date
-     * @param clientID This is the fifth parameter for a client id
+     * @param signupDate This is the third parameter for a signup date
+     * @param clientID This is the fourth parameter for a client id
      */
     public BankClient(final Name name, final Date birthDate, final Date signupDate, final String clientID)
     {
         this.name = name;
         this.birthDate = birthDate;
-        this.deathDate = null;
         this.signupDate = signupDate;
         this.clientID = clientID;
+        this.deathDate = null;
     }
 
     /**
--- src/code/ca/bcit/comp2522/bank/Main.java
+++ src/code/ca/bcit/comp2522/bank/Main.java
@@ -4,12 +4,14 @@ public class Main
 {
     public static void main (final String [] args)
     {
-
         // Name n1 = new Name("Bennett", "Lazarro");
         // System.out.println(n1.getFirst());
         BankClient b1;
         b1 = new BankClient(new Name("Bennett", "Lazarro"), new Date(2025,9,2), new Date(2025,8,2), "562355");
         System.out.println(b1.getDetails());
 
+        BankClient b2;
+        b2 = new BankClient(new Name("Abdullah", "Munawar"), new Date(2025,9,2), new Date(2025,8,2), "562355", new Date(2025,9,2));
+        System.out.println(b2.getDetails());
     }
 }
